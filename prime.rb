09c05f1a7b4a92af55d48f4588b1347c11c8b6bd
Emily Bolten <emilybolten@gmail.com>
2019-05-06 13:56:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+
+def prime(num)
+  nums = [1,2,3,4,5,6,7,8,9]
+
+  for i in 0..nums.length
+    if num % nums[i] == 0
+      return false
+    else
+      return true
+    end
+  end
