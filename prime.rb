dea08c66fe9d28502fe620639273a7d7ec3dabff
Emily Bolten <emilybolten@gmail.com>
2019-05-06 14:23:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@
 def prime?(num)
   if num >= 0
     (2..num-1).none? {|i| num % i == 0}
+  
+  elsif num < 0
+    (-2..num+1).none? {|i| num % i == 0}
   end
-elsif num < 0
-  (-2..num+1).none? {|i| num % i == 0}
-end
 end
