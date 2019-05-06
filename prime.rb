b2c9c756c5f495c91f90d40e6e689693a660f807
Emily Bolten <emilybolten@gmail.com>
2019-05-06 14:21:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,5 @@
 # Add  code here!
 
 def prime?(num)
-  nums = [1,2,3,4,5,6,7,8,9]
-
-    if num >= 0
-      for i in 2..num-1
-        if num % i == 0
-          return false
-          break
-        end
-      end
-    end
-
-    if num < 0
-      for i in -2..num+1
-        if num % i == 0
-          return false
-          break
-        end
-      end
-    end
-
-  return true
+    (2..num-1).each {|i| num % i == 0}
 end
