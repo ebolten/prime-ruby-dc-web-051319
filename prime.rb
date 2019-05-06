eb53eab718ed5b02b403e353769d078c7d18b304
Emily Bolten <emilybolten@gmail.com>
2019-05-06 14:17:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,10 +3,21 @@
 def prime?(num)
   nums = [1,2,3,4,5,6,7,8,9]
 
-    for i in 2..num-1
-      if num % i == 0
-        return false
-        break
+    if num >= 0
+      for i in 2..num-1
+        if num % i == 0
+          return false
+          break
+        end
+      end
+    end
+
+    if num < 0
+      for i in -2..num+1
+        if num % i == 0
+          return false
+          break
+        end
       end
     end
 
