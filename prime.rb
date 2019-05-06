558b3fb1e10fd0257418a1474b789ff050494075
Emily Bolten <emilybolten@gmail.com>
2019-05-06 13:57:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,10 @@
 
 def prime(num)
   nums = [1,2,3,4,5,6,7,8,9]
-
+  count = 0
   for i in 0..nums.length
     if num % nums[i] == 0
-      return false
-    else
-      return true
+      count += 1
     end
   end
 end
